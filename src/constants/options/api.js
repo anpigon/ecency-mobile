3b7b19e75e035b5c0b9c2102edca4bb0f5299b56
anpigon <markan82@gmail.com>
2022-12-02 23:35:21 +0900

    update: steemit api server list

--- src/constants/options/api.js
+++ src/constants/options/api.js
@@ -15,10 +15,22 @@ export const VALUE = [
 ];
 
 export const SERVER_LIST = [
-  'https://rpc.ecency.com',
-  'https://api.hive.blog',
-  'https://api.deathwing.me',
-  'https://anyx.io',
-  'https://api.pharesim.me',
-  'https://rpc.ausbit.dev',
+  'https://api.steemit.com',
+  'https://api.steemitdev.com',
+  'https://api.upvu.org',
+  'https://api.steem.fans',
+  'https://steemd.steemworld.org',
+  'https://api.justyy.com',
+  'https://cn.steems.top',
+  'https://steemapi.boylikegirl.club',
+  'https://steem.61bts.com',
+  'https://api.steemzzang.com',
+  'https://api.dlike.io',
+  'https://steemapi.3dkrender.com',
+  'https://api.steemwow.com',
+  'https://api.steem-fanbase.com',
+  'https://api.wherein.io',
+  'https://api.pennsif.net',
+  'https://api.campingclub.me',
+  'https://api.steem.buzz',
 ];
