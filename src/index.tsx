cf7cd9d8cd70782e7dc00e294a76196fcc91127e
anpigon <markan82@gmail.com>
2022-12-07 00:15:50 +0900

    refactor: src/index.tsx and flattenMessages.js

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable react/function-component-definition */
 import React from 'react';
-import {Provider, connect} from 'react-redux';
+import {StyleSheet} from 'react-native';
+import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
 import {IntlProvider} from 'react-intl';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
@@ -10,32 +13,31 @@ import {flattenMessages} from './utils/flattenMessages';
 import messages from './config/locales';
 
 import Application from './screens/application';
-import {persistor, store} from './redux/store/store';
+import {persistor, RootState, store} from './redux/store/store';
 import {initQueryClient} from './providers/queries';
+import {useAppSelector} from './hooks';
 
 const queryClientProviderProps = initQueryClient();
 
-const _renderApp = ({locale}) => (
-  <PersistQueryClientProvider {...queryClientProviderProps}>
-    <PersistGate loading={null} persistor={persistor}>
-      <IntlProvider locale={locale} messages={flattenMessages(messages[locale])}>
-        <GestureHandlerRootView style={{flex: 1}}>
-          <SafeAreaProvider>
-            <Host>
-              <Application />
-            </Host>
-          </SafeAreaProvider>
-        </GestureHandlerRootView>
-      </IntlProvider>
-    </PersistGate>
-  </PersistQueryClientProvider>
-);
-
-const mapStateToProps = state => ({
-  locale: state.application.language,
-});
-
-const App = connect(mapStateToProps)(_renderApp);
+const App: React.FC = () => {
+  const locale: string = useAppSelector((state: RootState) => state.application.language);
+  return (
+    <PersistQueryClientProvider {...queryClientProviderProps}>
+      <PersistGate loading={null} persistor={persistor}>
+        {/* @ts-ignore */}
+        <IntlProvider locale={locale} messages={flattenMessages((messages as any)[locale])}>
+          <GestureHandlerRootView style={styles.container}>
+            <SafeAreaProvider>
+              <Host>
+                <Application />
+              </Host>
+            </SafeAreaProvider>
+          </GestureHandlerRootView>
+        </IntlProvider>
+      </PersistGate>
+    </PersistQueryClientProvider>
+  );
+};
 
 export default function Root() {
   return (
@@ -44,3 +46,9 @@ export default function Root() {
     </Provider>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
--- src/utils/flattenMessages.ts
+++ src/utils/flattenMessages.ts
@@ -1,5 +1,5 @@
-export const flattenMessages = (nestedMessages, prefix = '') =>
-  Object.keys(nestedMessages).reduce((messages, key) => {
+export const flattenMessages = (nestedMessages: Record<string, any>, prefix = '') =>
+  Object.keys(nestedMessages).reduce<Record<string, string>>((messages, key) => {
     const value = nestedMessages[key];
     const prefixedKey = prefix ? `${prefix}.${key}` : key;
 
